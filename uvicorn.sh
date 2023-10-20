6bd257b72daaa3f87c9a00f769b11075b2d31476
Christos Alexiou <pcnoic@gmail.com>
2023-10-20 13:43:13 +0300

    Refactor api

--- uvicorn.sh
+++ uvicorn.sh
@@ -1,3 +1,3 @@
 set -e
 echo "Starting application..."
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
+uvicorn main:app --ws-ping-interval 5 --ws-ping-timeout 10 --forwarded-allow-ips "*", --host 0.0.0.0 --port ${PORT:-8000}
